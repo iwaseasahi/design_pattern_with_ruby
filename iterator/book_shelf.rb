dcfa11c2a30262f0ee78d60521fb29a509c11139
岩瀬旭 <a-iwase@shikaku-square.com>
2018-11-27 08:58:56 +0900

    iterator を実装する

--- iterator/book_shelf.rb
+++ iterator/book_shelf.rb
@@ -1,2 +1,23 @@
-class BookShelf < Array
+require_relative '../iterator/iterator'
+
+class BookShelf < Iterator
+  def initialize
+    @books = []
+  end
+
+  def get_book_at(index)
+    @books[index]
+  end
+
+  def append_book(book)
+    @books << book
+  end
+
+  def get_length
+    @books.length
+  end
+
+  def iterator
+    Iterator.new(self)
+  end
 end
--- iterator/main.rb
+++ iterator/main.rb
@@ -3,8 +3,13 @@ require_relative 'book_shelf'
 
 class Main
   book_shelf = BookShelf.new
-  book_shelf.push(Book.new('本1'))
-  book_shelf.push(Book.new('本2'))
-  book_shelf.push(Book.new('本3'))
-  book_shelf.each { |book| puts book.name }
+  book_shelf.append_book(Book.new('本1'))
+  book_shelf.append_book(Book.new('本2'))
+  book_shelf.append_book(Book.new('本3'))
+
+  iterator = book_shelf.iterator
+  while iterator.has_next?
+    book = iterator.next
+    puts book.name
+  end
 end
