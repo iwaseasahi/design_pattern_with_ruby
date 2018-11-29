bb8a8bc40524623dd21c519f91691d7938d4e9d5
岩瀬旭 <a-iwase@shikaku-square.com>
2018-11-29 09:02:31 +0900

    正しい意味を表す複数形にする

--- iterator/book_shelf_iterator.rb
+++ iterator/book_shelf_iterator.rb
@@ -1,17 +1,17 @@
 require_relative '../iterator/iterator'
 
 class BookShelfIterator < Iterator
-  def initialize(book)
-    @book = book
+  def initialize(books)
+    @books = books
     @index = 0
   end
 
   def has_next?
-    @index < @book.get_length
+    @index < @books.get_length
   end
 
   def next
-    book = self.has_next? ? @book.get_book_at(@index) : nil
+    book = self.has_next? ? @books.get_book_at(@index) : nil
     @index += 1
     book
   end
