0e1521ba50b148bc90e2ae4977588c7500be1d3c
岩瀬旭 <a-iwase@shikaku-square.com>
2018-11-30 09:20:14 +0900

    PrintBanner を実装する

---
+++ adapter_using_inheritance/print_banner.rb
@@ -0,0 +1,15 @@
+require_relative 'banner'
+
+class PrintBanner < Banner
+  def initialize(string)
+    super(string)
+  end
+
+  def print_weak
+    show_with_paren
+  end
+
+  def print_strong
+    show_with_aster
+  end
+end
