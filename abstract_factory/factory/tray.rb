d397b75f274fb71284c83f55c64b31ca31488d77
岩瀬旭 <a-iwase@shikaku-square.com>
2018-12-07 09:30:31 +0900

    Tray を実装する

--- abstract_factory/factory/tray.rb
+++ abstract_factory/factory/tray.rb
@@ -1,3 +1,14 @@
-class Tray
+require_relative 'item'
 
+module Factory
+  class Tray < Item
+    def initialize(caption)
+      super(caption)
+      @tray = []
+    end
+
+    def add(item)
+      @tray << item
+    end
+  end
 end
