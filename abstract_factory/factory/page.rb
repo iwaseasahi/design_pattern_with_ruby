34e1f9a51ccf2c2836d8af1b632bfe3c0e7f2bea
岩瀬旭 <a-iwase@shikaku-square.com>
2018-12-07 09:36:37 +0900

    Page を実装する

--- abstract_factory/factory/page.rb
+++ abstract_factory/factory/page.rb
@@ -1,3 +1,27 @@
-class Page
+module Factory
+  class Page
+    attr_reader :title, :author, :content
 
+    def initialize(title, author)
+      @title = title
+      @author = author
+      @content = []
+    end
+
+    def add(item)
+      @content << item
+    end
+
+    def output
+      filename = @title + '.html'
+      File.open(filename, 'w') do |f|
+        f.puts(self.make_html)
+      end
+      puts(filename + ' を作成しました。')
+    end
+
+    def make_html
+      raise NotImplementedError.new("You must implement #{self.class}##{__method__}")
+    end
+  end
 end
