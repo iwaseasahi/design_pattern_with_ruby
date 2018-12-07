fb1b84f2232be91826957e4999ae3fb7b1d5c056
岩瀬旭 <a-iwase@shikaku-square.com>
2018-12-07 09:22:46 +0900

    Item を実装する

--- abstract_factory/factory/item.rb
+++ abstract_factory/factory/item.rb
@@ -1,3 +1,13 @@
-class Item
+module Factory
+  class Item
+    attr_reader :caption
 
+    def initialize(caption)
+      @caption = caption
+    end
+
+    def make_html
+      raise NotImplementedError.new("You must implement #{self.class}##{__method__}")
+    end
+  end
 end
