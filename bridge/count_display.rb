c578c28aad1e7b1e7f4dc8f6519e13b03c28bff8
岩瀬旭 <a-iwase@shikaku-square.com>
2018-12-14 09:29:36 +0900

    CountDisplay を実装する

--- bridge/count_display.rb
+++ bridge/count_display.rb
@@ -1,3 +1,13 @@
-class CountDisplay
+require_relative 'display'
 
+class CountDisplay < Display
+  def initialize(impl)
+    super(impl)
+  end
+
+  def multi_display(times)
+    open
+    times.times { print }
+    close
+  end
 end
