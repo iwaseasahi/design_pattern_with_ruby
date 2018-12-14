42eb1c1db359e114b13b42c04e890a1e7f5bd2ea
岩瀬旭 <a-iwase@shikaku-square.com>
2018-12-14 09:30:51 +0900

    DisplayImpl を実装する

--- bridge/display_impl.rb
+++ bridge/display_impl.rb
@@ -1,3 +1,13 @@
 class DisplayImpl
+  def raw_open
+    raise NotImplementedError.new("You must implement #{self.class}##{__method__}")
+  end
 
+  def raw_print
+    raise NotImplementedError.new("You must implement #{self.class}##{__method__}")
+  end
+
+  def raw_close
+    raise NotImplementedError.new("You must implement #{self.class}##{__method__}")
+  end
 end
