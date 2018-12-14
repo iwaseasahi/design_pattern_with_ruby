0f28d626d0b5c31fc1ebea67bee10d2b5de92ae1
岩瀬旭 <a-iwase@shikaku-square.com>
2018-12-14 09:31:07 +0900

    StringDisplayImpl を実装する

--- bridge/string_display_impl.rb
+++ bridge/string_display_impl.rb
@@ -1,3 +1,28 @@
-class StringDisplayImpl
+require_relative 'display_impl'
 
+class StringDisplayImpl < DisplayImpl
+  def initialize(string)
+    @string = string
+    @width = string.encode('EUC-JP').bytesize
+  end
+
+  def raw_open
+    print_line
+  end
+
+  def raw_print
+    print('|' + @string + "|\n")
+  end
+
+  def raw_close
+    print_line
+  end
+
+  private
+
+  def print_line
+    print('+')
+    @width.times { print('-') }
+    print("+\n")
+  end
 end
