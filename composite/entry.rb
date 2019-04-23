780ad88b93d7845b6170990c75b5cfb41d5b4716
岩瀬旭 <a-iwase@shikaku-square.com>
2019-04-23 14:05:31 +0900

    folder に add できないよう例外を発生させる

--- composite/entry.rb
+++ composite/entry.rb
@@ -1,3 +1,5 @@
+require_relative 'file_treatment_exception'
+
 class Entry
   def get_name
     raise NotImplementedError.new("You must implement #{self.class}##{__method__}")
@@ -5,6 +7,10 @@ class Entry
 
   def get_size
     raise NotImplementedError.new("You must implement #{self.class}##{__method__}")
+  end
+
+  def add(entry)
+    raise FileTreatmentException.new
   end
 
   def print_list(prefix)
--- composite/file_treatment_exception.rb
+++ composite/file_treatment_exception.rb
@@ -1,3 +1,1 @@
-class FileTreatmentException
-
-end
+class FileTreatmentException < StandardError; end
