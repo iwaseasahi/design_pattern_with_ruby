7e0a556bf35cb39bb34a2414dd391d51eedb18aa
岩瀬旭 <a-iwase@shikaku-square.com>
2019-04-23 09:26:57 +0900

    composite を実装する

--- composite/directory.rb
+++ composite/directory.rb
@@ -1,3 +1,27 @@
-class Directory
+require_relative 'entry'
 
+class Directory < Entry
+  def initialize(name)
+    @name = name
+    @directory = []
+  end
+
+  def get_name
+    @name
+  end
+
+  def get_size
+    size = 0
+    @directory.each { |item| size += item.get_size }
+    size
+  end
+
+  def add(entry)
+    @directory << entry
+  end
+
+  def print_list(prefix)
+    puts(prefix + '/' + to_string)
+    @directory.each { |item| item.print_list(prefix + '/' + get_name) }
+  end
 end
--- composite/main.rb
+++ composite/main.rb
@@ -1,3 +1,31 @@
-class Main
+require_relative 'directory'
+require_relative 'folder'
 
+class Main
+  puts 'Making root entries...'
+  rootdir = Directory.new('root')
+  bindir = Directory.new('bin')
+  tmpdir = Directory.new('tmp')
+  usrdir = Directory.new('usr')
+  rootdir.add(bindir)
+  rootdir.add(tmpdir)
+  rootdir.add(usrdir)
+  bindir.add(Folder.new('vi', 10000))
+  bindir.add(Folder.new('latex', 20000))
+  rootdir.print_list( ' ')
+
+  puts ''
+  puts 'Making user entries...'
+  yuki = Directory.new('yuki')
+  hanako = Directory.new('hanako')
+  tomura = Directory.new('tomura')
+  usrdir.add(yuki)
+  usrdir.add(hanako)
+  usrdir.add(tomura)
+  yuki.add(Folder.new('dialy.html', 100))
+  yuki.add(Folder.new('Composite.java', 200))
+  hanako.add(Folder.new('memo.txt', 300))
+  tomura.add(Folder.new('game.doc', 400))
+  tomura.add(Folder.new('junk.mail', 500))
+  rootdir.print_list(' ')
 end
